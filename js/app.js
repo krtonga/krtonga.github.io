1d44052c6e23030830ff140eba4f4c2deaa3239d
Kristen Tonga <kristen.tonga@gmail.com>
2014-07-04 10:04:05 -0400

    added apple functionality... working on timing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 
+ $(document).ready(function(){
+
+    $('#treeAndMeBW').lazylinepainter({
+        "svgData": treeAndMe,
+        "strokeWidth": 2,
+        "strokeColor": "#e09b99",
+    }).lazylinepainter('paint');
+    $('#treeLines').lazylinepainter({
+            "svgData": treeLines,
+            "strokeWidth": 2,
+            "strokeColor": "#d3d3d3",
+            "strokeDash": "--.",
+            "delay":1000
+        }).lazylinepainter('paint');
+   $('#roots').lazylinepainter({
+      "svgData": root,
+      "strokeWidth": 2,
+      "strokeColor": "#e09b99"
+    }).lazylinepainter('paint')
+
+   $( ".roots-blob" ).delay(2000).show("fade", 2000, function() {
+    $( ".text" ).show("drop", 2000);
+     $( ".portfolio-blob" ).delay(2000).show("fade", 2000, function(){
+
+        $('#apple').lazylinepainter({
+          "svgData": apple,
+          "strokeWidth": 1,
+          "strokeColor": "#000000"
+        }).lazylinepainter('paint');
+          $( ".paper" ).show("drop", {direction:"up"}, 1000, function(){
+        $( ".code" ).show("drop", {direction: "up"}, 1000)
+      })
+     });
+   });
+});
+
